6b24db8d6636ee81df232b945af6be221595d703
papercut <papercut@172.17.60.184>
2019-08-13 14:27:35 +0700

    3commit

--- src/chap3/CallEmployee.java
+++ src/chap3/CallEmployee.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication1;
+package chap3;
 
 /**
  *
  * @author papercut
  */
-public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class CallEmployee {
     
 }
