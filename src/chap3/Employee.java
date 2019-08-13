968fa7512ab153728d07836b63df94c9daf7e745
papercut <papercut@172.17.60.184>
2019-08-13 16:31:25 +0700

    5commit

--- src/chap3/Employee.java
+++ src/chap3/Employee.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chap3;
-
-/**
- *
- * @author papercut
- */
 public class Employee {
+    static int id = 1001;
+    String name ="watcharapong";
+    int age = 35;
+    
+    static void work(){
+        System.out.println("work()");
+    }
+    
+    void getSalary(){
+        System.out.println("getSalary()");
+    }
+    
+    public static void main(String[] args) {
+        //call static
+        System.out.println("id :"+id);
+        work();
+        //non static
+       Employee em = new Employee();
+        System.out.println("Name:"+em.name);
+        System.out.println("Age:"+em.age);
+        em.getSalary();     
+    }
     
 }
